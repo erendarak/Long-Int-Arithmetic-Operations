2ba86c86cc4cdbb7a63d6620020f498bbae06053
Eren Darak <eren.darak@ozu.edu.tr>
2023-10-31 22:19:51 +0300

    Added new Main type. Fixed multiplication method. Just division method left.

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,109 @@
-import java.util.Scanner;
-
 public class Main {
     /**
-     * This is the test class of our project. There will be created link lists and objects to test it.
+     * This is the test class of our project. There will be created link lists with given long numbers in it and 4 operations to test them.
      * @param args
      */
     public static void main(String[] args){
 
-        System.out.println("Give first number");
+        System.out.println("-------------------------------------------------------------------");
 
-        Scanner scanner = new Scanner(System.in);
-        String given1 = scanner.nextLine();
+        String given1 = "3";
         LongNumber longNumber1 = new LongNumber(given1);
 
-        System.out.println("Give second number");
+        System.out.println("Given number 1: " + given1);
 
-        String given2 = scanner.nextLine();
+        String given2 = "5";
         LongNumber longNumber2 = new LongNumber(given2);
+
+        System.out.println("Given number 2: " + given2);
 
         LinkedList linkedList1 = new LinkedList();
         LinkedList linkedList2 = new LinkedList();
 
         Operations operations = new Operations(longNumber1, longNumber2, linkedList1, linkedList2);
 
-        System.out.println("==== l1 inverted ====");
+        System.out.println("==== linkedList1 reversed ====");
         System.out.println(linkedList1.toString());
-
-        System.out.println("==== l2 inverted ====");
+        System.out.println("==== linkedList2 reversed ====");
         System.out.println(linkedList2.toString());
 
-        System.out.println(":::\nPress what operation do you want to use\n 1-Addition (Press 1)\n 2-Subtraction (Press 2)\n 3-Multiplication (Press 3)\n:::");
-        int givenInt = scanner.nextInt();
+        System.out.println("==== Multiplication ====");
+        System.out.println(operations.Multiplication());
 
-        switch (givenInt){
-            case 1:
-                System.out.println("==== addition regular ====");
-                System.out.println(operations.Addition());
-                break;
-            case 2:
-                System.out.println("==== subtraction regular ====");
-                System.out.println(operations.Subtraction());
-                break;
-            case 3:
-                System.out.println("==== multiplication regular ====");
-                System.out.println(operations.Multiplication());
-                break;
-        }
+        System.out.println("==== Addition ====");
+        System.out.println(operations.Addition());
+
+        System.out.println("==== Subtraction ====");
+        System.out.println(operations.Subtraction());
+
+
+        System.out.println("-------------------------------------------------------------------");
+
+        String given3 = "350";
+        LongNumber longNumber3 = new LongNumber(given3);
+
+        System.out.println("Given number 3: " + given3);
+
+        String given4 = "500";
+        LongNumber longNumber4 = new LongNumber(given4);
+
+        System.out.println("Given number 2: " + given4);
+
+        LinkedList linkedList3 = new LinkedList();
+        LinkedList linkedList4 = new LinkedList();
+
+        Operations operations2 = new Operations(longNumber3, longNumber4, linkedList3, linkedList4);
+
+        System.out.println("==== linkedList3 reversed ====");
+        System.out.println(linkedList3.toString());
+        System.out.println("==== linkedList4 reversed ====");
+        System.out.println(linkedList4.toString());
+
+        System.out.println("==== Multiplication ====");
+        System.out.println(operations2.Multiplication());
+
+        System.out.println("==== Addition ====");
+        System.out.println(operations2.Addition());
+
+        System.out.println("==== Subtraction ====");
+        System.out.println(operations2.Subtraction());
+
+        System.out.println("-------------------------------------------------------------------");
+
+        String given5 = "827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689827949805639068390489568982794980563906839048956898279498056390683904895689";
+        LongNumber longNumber5 = new LongNumber(given5);
+
+        System.out.println("Given number 5: " + given5);
+
+        String given6 = "8279498056390683904895689";
+        LongNumber longNumber6 = new LongNumber(given6);
+
+        System.out.println("Given number 6: " + given6);
+
+        LinkedList linkedList5 = new LinkedList();
+        LinkedList linkedList6 = new LinkedList();
+
+        Operations operations3 = new Operations(longNumber5, longNumber6, linkedList5, linkedList6);
+
+        System.out.println("==== linkedList5 reversed ====");
+        System.out.println(linkedList5.toString());
+        System.out.println("==== linkedList6 reversed ====");
+        System.out.println(linkedList6.toString());
+
+        LinkedList linkedList7 = new LinkedList();
+        LinkedList linkedList8 = new LinkedList();
+
+        Operations operations4 = new Operations(longNumber5, longNumber6, linkedList7, linkedList8);
+
+        System.out.println("==== Addition ====");
+        System.out.println(operations4.Addition());
+
+        System.out.println("==== Multiplication ====");
+        System.out.println(operations3.Multiplication());
+
+        System.out.println("==== Subtraction ====");
+        System.out.println(operations4.Subtraction());
+
+
     }
 }
